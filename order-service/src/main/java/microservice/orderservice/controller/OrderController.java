cd4bfc1a97a5279c9c3bba26cddcc43fa88cf39c
MinChangJeong <wjdalsckd777@naver.com>
2024-07-25 22:41:18 +0900

    ip 출력 추가

--- order-service/src/main/java/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/microservice/orderservice/controller/OrderController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1")
 public class OrderController {
-    @GetMapping
+
+    @GetMapping("/order")
     public String getOrder() {
-        return "order";
+        try {
+            String ipAddress = InetAddress.getLocalHost().getHostAddress();
+            return "order:[" + ipAddress + "]";
+        } catch (UnknownHostException e) {
+            return "order:[Unable to determine IP address]";
+        }
     }
 }
--- product-service/src/main/java/microservice/productservice/controller/ProductController.java
+++ product-service/src/main/java/microservice/productservice/controller/ProductController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
+
     @GetMapping
     public String getProduct() {
-        return "product";
+        try {
+            String ipAddress = InetAddress.getLocalHost().getHostAddress();
+            return "product:[" + ipAddress + "]";
+        } catch (UnknownHostException e) {
+            return "product:[Unable to determine IP address]";
+        }
     }
 }
